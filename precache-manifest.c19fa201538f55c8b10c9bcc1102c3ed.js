94cb354d28ba86f12558ea6c0e86d71335610f1e
Cristian F. Tovar <ctovar@abexuspr.com>
2020-08-27 16:08:18 -0500

    Updates

---
+++ precache-manifest.c19fa201538f55c8b10c9bcc1102c3ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb1c00ed1b687ac518fb38b9f3ce459",
+    "url": "/treeMap/index.html"
+  },
+  {
+    "revision": "d8970910b1225147449b",
+    "url": "/treeMap/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e28fb0775d2e64bca84",
+    "url": "/treeMap/static/js/2.d785b272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/treeMap/static/js/2.d785b272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8970910b1225147449b",
+    "url": "/treeMap/static/js/main.a2f512b7.chunk.js"
+  },
+  {
+    "revision": "1bfcc2e8687d91494c88",
+    "url": "/treeMap/static/js/runtime-main.14a31fca.js"
+  }
+]);
